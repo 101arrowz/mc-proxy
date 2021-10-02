b52f40c941896af6b37c838c39ac5bc17739ec4b
101arrowz <arjunbarrett@gmail.com>
2021-10-02 03:25:28 -0700

    fixes

--- src/connection/packets/play.rs
+++ src/connection/packets/play.rs
@@ -2,14 +2,16 @@ use crate::{connection::{
     codec::{IncomingPacket, OutgoingPacket},
     error::Error,
     Client, ServerConnection, State,
-}, protocol::types::{Decode, LengthCappedString}};
+}, protocol::types::{Decode, LengthCappedString, VarInt}};
 use crate::protocol::{types::{Encode, Chat, UUID}, version::ProtocolVersion};
 use std::{
-    convert::Infallible,
-    future::{ready, Future, Ready},
     rc::Rc,
-    cell::RefCell
+    cell::RefCell,
+    borrow::Cow,
+    collections::HashMap,
+    io::Cursor
 };
+use futures::future::join_all;
 use tokio::{
     io::{copy, AsyncReadExt, AsyncWriteExt},
     net::tcp::{OwnedReadHalf, OwnedWriteHalf},
@@ -165,6 +167,7 @@ impl ServerConnection {
             } = client;
             let version = *version;
             let send_to_client = Rc::new(RefCell::new(Vec::new()));
+            let all_local_players = Rc::new(RefCell::new(HashMap::<UUID, String>::new()));
             let ServerConnection {
                 inbound: server_inbound,
                 outbound: server_outbound,
@@ -172,6 +175,7 @@ impl ServerConnection {
                 ..
             } = self;
             let server_version = *server_version;
+            let client = reqwest::Client::new();
             try_join!(
                 async {
                     // loop {
@@ -188,9 +192,57 @@ impl ServerConnection {
                             1 => {
                                 let msg = LengthCappedString::<256>::decode(&mut packet.content, server_version).await?;
                                 packet.content.finished()?;
-                                if msg.0 == "/hello_world" {
-                                    send_to_client.borrow_mut().push(Chat::Raw("hello world!".into()));
-                                } else {
+                                if msg.0.starts_with("/stats ") {
+                                    let uname = &msg.0[7..];
+                                    let players = if uname == "*" {
+                                        all_local_players.borrow().iter().map(|(_, v)| Cow::Owned(v.into())).collect()
+                                    } else {
+                                        Vec::from([Cow::Borrowed(uname)])
+                                    };
+                                    for good_player in join_all(players.into_iter().map(|player| {
+                                        let client = &client;
+                                        let send_to_client = send_to_client.clone();
+                                        async move {
+                                            let mut out = None;
+                                            let mut result: Cow<'_, str>;
+                                            // todo: optimize
+                                            let resp: serde_json::Value = client.clone()
+                                                .get(["https://api.mojang.com/users/profiles/minecraft/", &player].concat())
+                                                .send().await?
+                                                .json().await.unwrap_or_default();
+                                            if let Some(uuid) = resp.get("id") {
+                                                let uuid = uuid.as_str().unwrap();
+                                                let resp: serde_json::Value = client
+                                                    .get("https://api.hypixel.net/player")
+                                                    .query(&[("uuid", uuid)])
+                                                    .header("API-Key", "defunct_key")
+                                                    .send().await?
+                                                    .json().await?;
+                                                result = "Unknown".into();
+                                                if let Some(player_info) = resp.get("player") {
+                                                    let bw_stats = &player_info["stats"]["Bedwars"];
+                                                    if bw_stats != &serde_json::Value::Null {
+                                                        let fkdr = bw_stats["final_kills_bedwars"].as_f64().unwrap_or(0.0) / bw_stats["final_deaths_bedwars"].as_f64().unwrap_or(1.0);
+                                                        if fkdr > 5.0 {
+                                                            out = Some((fkdr, player.to_string()));
+                                                        }
+                                                        result = format!("{:.2}", fkdr).into();
+                                                    }
+                                                }
+                                            } else {
+                                                result = "Nicked".into()
+                                            }
+                                            send_to_client.borrow_mut().push(Chat::Raw([&player, ": ", &result].concat().into()));
+                                            Ok::<Option<(f64, String)>, Error>(out)
+                                        }
+                                    })).await {
+                                        if let Some((fkdr, name)) = good_player? {
+                                            let mut out_packet = outbound.create_packet(1, None).await?;
+                                            LengthCappedString::<256>(format!("[QDodge Bot] {} has {:.2} FKDR", name, fkdr).into()).encode(&mut out_packet, version).await?;
+                                            out_packet.shutdown().await?;
+                                        }
+                                    }
+                                } else {  
                                     let mut out_packet = outbound.create_packet(packet.id, Some(packet.len)).await?;
                                     msg.encode(&mut out_packet, version).await?;
                                     out_packet.shutdown().await?;
@@ -216,14 +268,54 @@ impl ServerConnection {
                             out_packet.shutdown().await?;
                         }
                         let mut packet = inbound.next_packet().await?;
-                        let mut buf = Vec::new();
-                        packet.content.read_to_end(&mut buf).await?;
-                        // let (_, _, buf) = dbg!(packet.id, packet.len, buf);
-                        let mut out_packet = server_outbound.create_packet(packet.id, Some(packet.len)).await?;
-                        // copy(&mut packet.content, &mut out_packet).await?;
-                        out_packet.write_all(&buf).await?;
-                        packet.content.finished()?;
-                        out_packet.shutdown().await?;
+                        match packet.id {
+                            0x38 => {
+                                let mut vec = Vec::with_capacity(packet.len);
+                                packet.content.read_to_end(&mut vec).await?;
+                                packet.content.finished()?;
+                                let mut content = Cursor::new(&vec);
+                                let action = VarInt::decode(&mut content, version).await?.0;
+                                let num_players = VarInt::decode(&mut content, version).await?.0;
+                                for _ in 0..num_players {
+                                    let uuid = UUID::decode(&mut content, version).await?;
+                                    match action {
+                                        0 => {
+                                            let name = LengthCappedString::<16>::decode(&mut content, version).await?.0;
+                                            all_local_players.borrow_mut().insert(uuid, name.into());
+                                            for _ in 0..VarInt::decode(&mut content, version).await?.0 {
+                                                LengthCappedString::<32767>::decode(&mut content, version).await?;
+                                                LengthCappedString::<32767>::decode(&mut content, version).await?;
+                                                if bool::decode(&mut content, version).await? {
+                                                    LengthCappedString::<32767>::decode(&mut content, version).await?;
+                                                }
+                                            }
+                                            VarInt::decode(&mut content, version).await?;
+                                            VarInt::decode(&mut content, version).await?;
+                                            if bool::decode(&mut content, version).await? {
+                                                Chat::decode(&mut content, version).await?;
+                                            }
+                                        },
+                                        4 => {
+                                            all_local_players.borrow_mut().remove(&uuid);
+                                        },
+                                        _ => {
+
+                                        }
+                                    }
+                                }
+                                content.set_position(0);
+                                let mut out_packet = server_outbound.create_packet(packet.id, Some(packet.len)).await?;
+                                copy(&mut content, &mut out_packet).await?;
+                                packet.content.finished()?;
+                                out_packet.shutdown().await?;
+                            },
+                            _ => {
+                                let mut out_packet = server_outbound.create_packet(packet.id, Some(packet.len)).await?;
+                                copy(&mut packet.content, &mut out_packet).await?;
+                                packet.content.finished()?;
+                                out_packet.shutdown().await?;
+                            }
+                        }
                     }
                     Ok::<(), Error>(())
                 }
